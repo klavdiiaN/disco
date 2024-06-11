d2008776b605fd01f732d9b9d011e9b0420f908a
tharvik <tharvik@users.noreply.github.com>
2024-06-11 17:15:24 +0200

    webapp/ImageCard: rm unused showButton

--- webapp/src/components/containers/ImageCard.vue
+++ webapp/src/components/containers/ImageCard.vue
@@ -1,7 +1,6 @@
 <template>
   <div
     class="grid grid-cols-1 w-full bg-white aspect-square rounded-xl drop-shadow-md hover:drop-shadow-xl transition duration-500 hover:scale-105 opacity-70 hover:opacity-100 shadow hover:shadow-lg"
-    :class="{ 'cursor-pointer': showButton }"
   >
     <div class="grid grid-cols-1 gap-1 text-center content-center p-2 h-16">
       <slot name="title" />
@@ -12,27 +11,15 @@
     <canvas class="h-full w-full object-fill rounded-b-lg" ref="canvas">
       image
     </canvas>
-    <div
-      v-if="showButton"
-      class="opacity-0 hover:opacity-100 duration-500 absolute inset-0 z-10 flex justify-center items-end text-white font-semibold p-5"
-    >
-      <CustomButton class="text-xs"> click to show on maps </CustomButton>
-    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { ref, onMounted, onUpdated } from "vue";
 
-import CustomButton from "@/components/simple/CustomButton.vue";
-
-const props = withDefaults(
-  defineProps<{
-    image: ImageData;
-    showButton: boolean;
-  }>(),
-  { showButton: false },
-);
+const props = defineProps<{
+  image: ImageData;
+}>();
 
 const canvas = ref<HTMLCanvasElement | null>(null);
 
